6d24f64ee3187d08f18433643785b5e7f8b3bcb4
MIkhailKuznetcov-135-2 <vodka2505@mail.ru>
2026-08-16 13:43:17 +0300

    fix: update ChromeDriver init for Selenium 4+ (service + options)

--- 06K_lesson_FIX/test_03_shop.py
+++ 06K_lesson_FIX/test_03_shop.py
@@ -4,11 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
 
